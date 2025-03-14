7c924ef9d529f3748f88678fd130183508a3c18b
Rackhar <91132270+rackhar@users.noreply.github.com>
2025-03-14 21:15:07 +0300

    Проверено, отлажено, в рабочем виде

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '7719839328:AAHH0rgIZTYkVWLUXCshCg9zqUE7vqgIvug'
+
+keys = {
+    'биткоин':'BTC',
+    'эфириум':'ETC',
+    'доллар':'USD',
+    '0xBitcoin':'0XBTC',
+    'FirstBlood':'1ST',
+    '1World':'1WO',
+
+}
